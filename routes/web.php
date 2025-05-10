9d0bf245be6bb2262b4631be4bfb44d07a8219a0
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-05-10 08:54:01 +0200

    dodanie autoryzacji

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,13 +13,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('ogolne.welcome');
-})->name('start'); 
+})->name('start');
 Route::get('/kontakt', function () {
     return view('ogolne.kontakt');
 })->name('kontakt'); 
 Route::get('/onas', function () {
-    return view('ogolne.onas');
-})->name('onas'); 
+    return view('ogolne.onas');})->name('onas'); 
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
